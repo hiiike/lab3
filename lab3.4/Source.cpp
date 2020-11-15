e20b3b485956d9b726395d696449c1f53c1613bc
hiiike <58051188+hiiike@users.noreply.github.com>
2020-11-15 20:53:48 +0200

    lab3.4(2)

--- lab3.4/Source.cpp
+++ lab3.4/Source.cpp
@@ -8,16 +8,22 @@ using namespace std;
 
 int main()
 {
-    double x; // вхідний аргумент
-    double y; // вхідний параметр
-    double R; // вхідний параметр
+    double x, y, R, x0, y0, x1, y1;
 
     cout << "R = "; cin >> R;
     cout << "x = "; cin >> x;
     cout << "y = "; cin >> y;
 
+    x0 = R;
+    y0 = 0;
+    x1 = -R;
+    y1 = 0;
+
+
     // розгалуження в повній формі
-    if (y <= (sqrt(pow(R,2) - (x + R) * (x + R))) || y <= (-sqrt(pow(R,2) - (x - R) * (x - R))))
+    if (((x > 0 && x < R) && (y < 0 && y > -R) && ((x - x0) * (x - x0) + (-y0) * (y - y0) <= R * R)) ||
+        (((x < 0 && x > -R) && (y > 0 && y < R)) && ((x - x1) * (x - x1) + (y - y1) * (y - y1))))
+
         cout << "yes" << endl;
     else
         cout << "no" << endl;
